7b312a3f13e64e96cb19dbaec436c968242c686d
=Anurag <=anuragsnd@gmail.com>
2025-05-11 00:45:46 +0530

    completed

--- main.py
+++ main.py
@@ -6,21 +6,44 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 import math
+
+timer = None
 
 
 # ---------------------------- TIMER RESET ------------------------------- #
+def reset_timer():
+    global reps
+    reps = 0
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    timer_label.config(text="Timer")
+    check_mark.config(text="")
 
-# ---------------------------- TIMER MECHANISM ------------------------------- # 
+
+# ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(WORK_MIN * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    shot_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        timer_label.config(text="BREAK", fg=RED)
+    elif reps % 2 == 0:
+        count_down(shot_break_sec)
+        timer_label.config(text="BREAK", fg=PINK)
+    else:
+        count_down(work_sec)
+        timer_label.config(text="WORK", fg=GREEN)
 
 
-# ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+# ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
 def count_down(count):
     count_min = math.floor(count / 60)
     count_sec = count % 60
@@ -29,7 +52,15 @@ def count_down(count):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        global timer
+        timer = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        marks = ""
+        work_sessions = math.floor(reps / 2)
+        for _ in range(work_sessions):
+            marks += "✔"
+        check_mark.config(text=marks)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -54,11 +85,11 @@ start_button.config(highlightthickness=0)
 start_button.grid(row=2, column=0)
 
 """reset_button"""
-reset_button = Button(text="Reset")
+reset_button = Button(text="Reset", command=reset_timer)
 reset_button.config(highlightthickness=0)
 reset_button.grid(row=2, column=2)
 """check_mark"""
-check_mark = Label(text="✔", fg=GREEN, bg=YELLOW, highlightthickness=0)
+check_mark = Label(fg=GREEN, bg=YELLOW, highlightthickness=0)
 check_mark.grid(row=3, column=1)
 
 window.mainloop()
